42981082f31c648293af31eb4a3285f2cf9b096d
Nathan Jaremko <nathan@jaremko.ca>
2020-11-16 21:46:44 -0500

    More

--- src/Relational/Data/Table.hs
+++ src/Relational/Data/Table.hs
@@ -6,8 +6,8 @@ where
 import qualified Data.Map as Map
 import Data.MultiSet (MultiSet)
 import qualified Data.MultiSet as MultiSet
-import Relude hiding (empty, filter, null, reduce)
 import qualified Relational.Data.Bag as Bag
+import Relude hiding (empty, filter, null, reduce)
 
 newtype Table k v = Table {unTable :: Map k (MultiSet v)}
 
@@ -26,8 +26,14 @@ isEmpty = Map.null
 single :: (k, v) -> Map k v
 single (k, v) = Map.singleton k v
 
-merge :: (Ord k, Enum k, Num k) => (Map k v1, Map k v2) -> Map k (v1, v2)
-merge (m1, m2) = Map.fromList (zip [0 ..] $ zip (Map.elems m1) (Map.elems m2))
+merge :: Ord k => (Map k v, Map k v) -> Map k (v, v)
+merge (m1, m2) = Map.unionWith combineTuples (duplicateValues m1) (duplicateValues m2)
+  where
+    duplicateValues :: Map k v -> Map k (v, v)
+    duplicateValues m = fmap (\x -> (x, x)) m
+
+    combineTuples :: (v, v) -> (v, v) -> (v, v)
+    combineTuples (x1, _) (y1, _) = (x1, y1)
 
 merge' :: Map k (v1, v2) -> (Map k v1, Map k v2)
 merge' x = (fmap fst x, fmap snd x)
@@ -37,6 +43,9 @@ dom m = MultiSet.fromList $ Map.keys m
 
 cod :: (Ord k, Ord v) => Map k v -> MultiSet v
 cod t = reduce (fmap MultiSet.singleton t)
+
+lookup :: Ord k => Map k v -> (k -> Maybe v)
+lookup = flip Map.lookup
 
 ix :: (Ord k, Ord v) => MultiSet (k, v) -> Map k (MultiSet v)
 ix kvs = Map.fromList ((\(k, v) -> (k, MultiSet.fromList [v])) <$> MultiSet.elems kvs)
