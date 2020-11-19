2625b4bba840320cdbd9ddc2a30970352d3c2395
Nathan Jaremko <nathan@jaremko.ca>
2020-11-18 22:06:58 -0500

    Progress

--- app/Main.hs
+++ app/Main.hs
@@ -7,20 +7,22 @@ import Relational
 import qualified Data.Map as Map
 import Data.MultiSet (MultiSet)
 import qualified Data.MultiSet as MultiSet
+import Relational.Data.Table (example)
 
 data Customer = C {cid :: Int, name :: Text}
 
 data Invoice = I {iid :: Int, cust :: Int, due :: Int, amount :: Int}
 
-example :: Bag Customer -> Bag Invoice -> Bag (Bag Text, Bag Int)
-example cs is =
-  fmap (pair (fmap name, fmap amount))
-    . cod
-    $ pair (identity, filterBag ((< 5) . due))
-      <$> merge (cs `indexBy` cid, is `indexBy` iid)
-  where
-    pair (f, g) (a, b) = (f a, g b)
+-- example :: Bag Customer -> Bag Invoice -> Bag (Bag Text, Bag Int)
+-- example cs is =
+--   fmap (pair (fmap name, fmap amount))
+--     . cod
+--     $ pair (identity, filterBag ((< 5) . due))
+--       <$> merge (cs `indexBy` cid, is `indexBy` iid)
+--   where
+--     pair (f, g) (a, b) = (f a, g b)
 
 main :: IO ()
 main = do
+  example
   return ()
--- src/Relational/Data/Table.hs
+++ src/Relational/Data/Table.hs
@@ -1,5 +1,7 @@
 module Relational.Data.Table
   ( Relation (..),
+    Algebra (..),
+    example,
   )
 where
 
@@ -11,46 +13,113 @@ import qualified Data.Vector as Vector
 import qualified Relational.Data.Bag as Bag
 import Relude hiding (empty, filter, null, reduce)
 
-newtype Header = Header {unHeader :: Map Text Int} deriving newtype (Eq)
+newtype Heading = Heading {unHeading :: Map Text Int} deriving newtype (Eq, Semigroup, Monoid)
 
-data Relation a = Relation Header (Vector a)
+newtype Tuple = Tuple {unTuple :: Vector Elem} deriving newtype (Eq, Semigroup, Monoid)
 
-data Elem = ElemText Text | ElemInt Int
+newtype Name = Name {unName :: Text} deriving newtype (Eq, Semigroup, Monoid, IsString)
 
-data Person = Person {personName :: Text, personEmal :: Text}
+data Relation a = Relation
+  { name :: Name,
+    heading :: Heading,
+    tuples :: Map a Tuple
+  }
+
+newtype PrimaryKeyNumeric = PrimaryKeyNumeric Int deriving newtype (Eq, Ord, Enum, Num)
+
+newtype PrimaryKeyText = PrimaryKeyText Text deriving newtype (Eq, Ord)
+
+data Elem
+  = ElemText Text
+  | ElemInt Int
+  | ElemBool Bool
+  | ElemBlob ByteString
+  | ElemVarChar Text Int
+  | ElemDecimal Float
+  deriving stock (Eq, Show)
 
 class Algebra a where
-  projection :: (a -> a) -> a -> a
+  projection :: Heading -> a -> a
   union :: a -> a -> Maybe a
   cartesianProduct :: a -> a -> a
-  selection :: Header -> a -> a
+  selection :: (a -> a) -> a -> a
+
+prettyPrint :: Relation a -> IO ()
+prettyPrint (Relation _ heading tuples) = do
+  let header = fst <$> headerByVal heading
+  putTextLn $ show header
+  traverse_ (\(Tuple x) -> print x) tuples
+
+example :: IO ()
+example = do
+  let heading = Heading $ Map.fromList [("id", 0), ("name", 1), ("email", 2)]
+      tuples =
+        Map.fromList $
+          zip
+            ([0 ..] :: [PrimaryKeyNumeric])
+            [ Tuple $ Vector.fromList [ElemInt 0, ElemText "John", ElemText "john.smith@gmail.com"],
+              Tuple $ Vector.fromList [ElemInt 1, ElemText "Adam", ElemText "adam.smith@gmail.com"]
+            ]
+      relation = Relation {name = "", heading, tuples}
+  prettyPrint $ cartesianProduct relation relation
+
+headerByVal :: Heading -> [(Text, Int)]
+headerByVal (Heading h) = sortWith snd $ Map.assocs h
 
 indexFilter ::
-  (Num b, Eq b, Enum b) =>
+  (Num b, Eq b) =>
   Vector a ->
   Vector b ->
   Vector a
-indexFilter v idx = Vector.map (fst) (Vector.filter (\x -> elemV (snd x) idx) vectorMap)
+indexFilter v idx = Vector.map fst (Vector.filter (\x -> elemV (snd x) idx) vectorMap)
   where
     vectorMap = Vector.zipWith (\a b -> (b, a)) (Vector.iterateN size (+ 1) 0) v
     size = Vector.length v
-    elemV a = Vector.foldl (\acc x -> if x == a then True else acc) False
+    elemV a = Vector.foldl (\acc x -> (x == a) || acc) False
 
-instance Algebra (Relation a) where
-  selection (Header selectedHeaders) (Relation (Header headers) rows) =
+instance Semigroup (Relation PrimaryKeyNumeric) where
+  (<>) = (<>)
+
+instance Monoid (Relation PrimaryKeyNumeric) where
+  mempty =
+    Relation
+      { name = mempty,
+        heading = mempty,
+        tuples = mempty
+      }
+
+instance Algebra (Relation PrimaryKeyNumeric) where
+  projection (Heading selectedHeaders) (Relation _ (Heading headers) rows) =
     let newHeader = Map.intersection selectedHeaders headers
-        newRows = indexFilter rows (Vector.fromList $ Map.elems newHeader)
-     in Relation (Header newHeader) newRows
+        newRows = Map.map (\(Tuple elems) -> Tuple $ indexFilter elems (Vector.fromList $ Map.elems newHeader)) rows
+     in Relation {name = "", heading = Heading newHeader, tuples = newRows}
 
-  union (Relation header1 rows1) (Relation header2 rows2) = 
-      if header1 == header2
-          then Just $ Relation header1 $ rows1 <> rows2
-          else Nothing
+  union (Relation _ header1 rows1) (Relation _ header2 rows2) =
+    if header1 == header2
+      then Just $ Relation {name = "", heading = header1,  tuples = rows1 <> rows2}
+      else Nothing
 
-  cartesianProduct 
+  cartesianProduct
+    (Relation (Name name1) (Heading h1) rel1)
+    (Relation (Name name2) (Heading h2) rel2) =
+      let e1 = (\(Tuple e) -> e) <$> Map.elems rel1
+          e2 = repeat $ (\(Tuple e) -> e) <$> Map.elems rel2
+          tuples =
+            let leftWithAllRight = \(a1, a2) -> fmap (a1 <>) a2
+             in mconcat (leftWithAllRight <$> zip e1 e2)
+          maxIndex = foldr max 0 h1
+          newHeading =
+            Heading
+              . Map.union h1 -- Make a new map of attribute index for both relations
+              . Map.fromList
+              . flip zip [maxIndex + 1 ..] -- Add new indexes for these attribute names
+              . fmap ((name2 <>) . fst) -- Take just the attribute names
+              . sortWith snd -- Sort by indexes
+              $ Map.assocs h2 -- Get indexes of attributes in relation
+       in Relation (Name name1) newHeading
+            . Map.fromList
+            . zip [0 ..]
+            . fmap Tuple
+            $ tuples
 
--- example :: Relation Elem
--- example = do
---   let x1 = Relation ["id", "name", "email"] $ Vector.fromList [ElemInt 1, ElemText "John", ElemText "Smith"]
---   let x2 = projection (\(Relation [h : headers] rows) -> Relation headers rows) x1
---   x2
+  selection func = func
